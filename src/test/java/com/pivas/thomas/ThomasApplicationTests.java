29a614450c5ad049f3a8ffa4f84d29e24c9c3927
GustavoPivatto <gustavo05032004@gmail.com>
2024-04-04 15:19:53 -0300

    first commit

---
+++ src/test/java/com/pivas/thomas/ThomasApplicationTests.java
@@ -0,0 +1,12 @@
+package com.pivas.thomas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThomasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
